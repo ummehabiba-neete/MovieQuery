110782c9f03299bab9a17a93110f2d88a523873a
cis101 <cis101@secs.iub.edu.bd>
2017-10-02 09:15:58 +0600

    movieQuery

--- movieQuery.js
+++ movieQuery.js
@@ -4,24 +4,28 @@ var movieList = [
   { title: "Omen : The Final Conflict", year: "1981", length: 108, rating: "6.7", genre: "horror" },
   { title: "Gladiator", year: "2000", length: 155, rating: "8.5", genre: "historical" },
   { title: "The Patriot", year: "2000", length: 165, rating: "7.1", genre: "historical" },
-  { title: "The Matrix Reloaded”, year: “2003", length: 138, rating: "7.2", genre: "sci-fi" }
+  { title: "The Matrix Reloaded”, year: “2003", length: 138, rating: "7.2", genre: "sci-fi" },
+  {title: "Treasure of Island", year:"1882" , length: 134, rating: "8.6",genre: "adventure"},
 ];
 
 function getMovieTitle( index ){
-  //add code
+  return movieList[index].title;
 }
 
 console.log(getMovieTitle(3));
+console .log (getMovieTitle(0));
 
 function addMovie( movie ){
-  //add code
+  movieList.push(movie);
+  return movieList;
+  
 }
 
 var newMovie = {};
 console.log(addMovie(newMovie));
 
 function movieByRating(){
-  //add code
+  
 }
 
 console.log(movieByRating());
